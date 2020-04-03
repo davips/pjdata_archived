7123b5ec0fa2790e58099a8ea4882473ac2586ee
ealcobaca <e.alcobaca@gmail.com>
2020-04-03 01:28:48 -0300

    updating more components

--- pjdata/step/transformation.py
+++ pjdata/step/transformation.py
@@ -5,47 +5,20 @@ from pjdata.aux.identifyable import Identifyable
 from pjdata.aux.serialization import serialize, deserialize
 
 
-class Transformation(Identifyable, ABC):
+class Transformation(Identifyable):
     def __init__(self, transformer, step):
-        """
-        Immutable application or use of a Transformer.
-        :param transformer: Transformer/Pipeline
-        :param step: 'a'pply or 'u'se
-        """
-        # Precisei retirar referência ao transformer, para que pickle parasse
-        # de dar problema ao carregar um objeto Data do PickleServer. Esse
-        # problema começar após a unificação ML com CS, sobrescrenvendo
-        # __new__ nos containeres. O erro acontecia quando o pickle tentava
-        # recriar Containeres do histórico de Data, mas, por algum motivo
-        # tentava fazê-lo sem transformers. Serializei config pelo mesmo motivo.
         if step is None:
             raise Exception(
                 'Operation cannot be None! Hint: self._transformation() '
                 'should be called only during apply() or use() steps!')
-        self.serialized = transformer.serialized
         self.step = step
-        self.name = transformer.name
-        self.path = transformer.path
-        self._config = serialize(transformer)
+        self._config = transformer.serialized
+        self._uuid = transformer.uuid
+
+    def _uuid_impl(self):
+        return self.step, self.uuid
 
     @property
     @lru_cache()
     def config(self):
         return deserialize(self._config)
-
-
-class NoTransformation(type):
-    transformer = None
-    step = None
-    name = None
-    path = None
-    config = None
-    from pjdata.aux.encoders import int2tiny
-    uuid = 'T' + int2tiny(0)
-
-    def __new__(cls, *args, **kwargs):
-        raise Exception(
-            'NoTransformation is a singleton and shouldn\'t be instantiated')
-
-    def __bool__(self):
-        return False
