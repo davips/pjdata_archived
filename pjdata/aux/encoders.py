460bfa23e6175fbb1f84360a633e5c08fc08882e
davips <dpsabc@gmail.com>
2020-05-10 23:41:26 -0300

    Restated slower UUID. It seems more sensible, since no differences were detected in benchmarks with automl-multi-obj.

--- pjdata/aux/encoders.py
+++ pjdata/aux/encoders.py
@@ -1,79 +1,35 @@
 import hashlib
-from json import JSONEncoder
 
-import numpy as np
-
-from pjdata.aux.alphabets import alphabet1224, alphabet1224dic
+from pjdata.aux.alphabets import alphabet800, alphabet800dic
 
 
-def md5digest(bytes_content):
-    """
+def md5_int(bytes_content):
+    """Return MD5 hash as integer.
+
     Generates a hash intended for unique identification of content
      (unique for any reasonably finite universe).
+
     It is preferred to generate such hash on compressed data,
     since MD5 is much slower for large data than the compression itself.
-    :param bytes_content:
+
+    Parameters
+    ----------
+    bytes_content
         encoded content; it can be a packed object, a text, JSON,...
-    :return: str <32 characters>
+
+    Returns
+    -------
+        a big integer in [0; 2^128[
     """
-    return hashlib.md5(bytes_content).digest()
+    return int.from_bytes(hashlib.md5(bytes_content).digest(), 'big')
 
 
-def digest2pretty(bytes_digest):
-    """
-    Convert MD5 representation (16 bytes) to a friendly still short one
-     (20 digits in base-107).
-    :param bytes_digest:
-    :return: string with 20 digits, padded with '0' when needed
-    """
-    return int2pretty(bytes2int(bytes_digest))
+def enc(number, alphabet=alphabet800, padding=14):
+    """Encode an integer to base-n. n = len(alphabet).
 
-
-def int2pretty(number):
-    """Convert number to a tiny human-friendly string (20 chars)."""
-    return enc(number).rjust(20, '0')
-
-
-def int2bytes(x):
-    return x.to_bytes(16, 'big')
-
-
-def bytes2int(digest):
-    """
-    Convert MD5 digest (16 bytes) to int.
-    :param digest:
-    :return: int
-    """
-    return int.from_bytes(digest, 'big')
-
-
-def dec(digest, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                         'abcdefghijklmnopqrstuvwxyz'
-                         'ÁÂÄÅÆÉÊËÍÎÏÐÑÓÔÖÚÛÜÝÞßáâäåæçéêëíîïðñóôöøúûüýþ'):
-    """
-    Decode digest from base-len(alphabet).
-    See enc() for more info.
-    :param digest:
-    :param alphabet:
-    :return:
-    """
-    res = 0
-    last = len(digest) - 1
-    # It didn't help too much to replace list by a dict. 7.8us vs 6.2us
-    base = len(alphabet)
-    for i, d in enumerate(digest):
-        res += alphabet.index(d) * pow(base, last - i)
-    return res
-
-
-def enc(big_number, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                             'abcdefghijklmnopqrstuvwxyz'
-                             'ÁÂÄÅÆÉÊËÍÎÏÐÑÓÔÖÚÛÜÝÞßáâäåæçéêëíîïðñóôöøúûüýþ'):
-    """
-    Encode an integer to base-n. n = len(alphabet).
-    The default is base107 since it is enough to represent MD5 as 18-19 chars.
-    The selected default alphabet contains only numbers and letters. Similar
-    letters were arbitrarily removed.
+    The default is base-800 since it is enough to represent MD5 as 18 chars in
+    utf8 (1-2 bytes each char).
+    The selected default alphabet contains only word characters.
     This alphabet is intended to be printable and to be free of
     disruptive characters, i.e. any combination of adjacent characters will be
     understood as a single word by most linux terminals and editors.
@@ -81,9 +37,9 @@ def enc(big_number, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
     The following list shows how the alphabet size relates to the number of
     necessary digits to represent the biggest MD5 number (2^128).
-    The hexdigest alredy uses 32 digits, so we want less than that.
+    The hexdigest already uses 32 digits, so we want less than that.
     According to the list below, good choices for the alphabet size would be in
-    the range 85-185, since values higher than 256 are outside latin1 range.
+    the range 85-185 to keep 1 byte for each (latin1 range).
 
     alphabet-size   number-of-digits   comments
     2 128 # crude md5 as binary string
@@ -128,19 +84,55 @@ def enc(big_number, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz
 
     :param alphabet: string with allowed digits
-    :param big_number: an integer, usually a big MD5-like one
-    :return: string representing a base-107 number (or any other base,
-    depending on the given alphabet length)"""
+    :param number: an integer, usually a big MD5-like one
+    :return: string representing a base-800 number (or any other base,
+    depending on the given alphabet length)
+
+    Parameters
+    ----------
+    number
+
+    alphabet
+
+    padding
+
+    Returns
+    -------
+
+    """
     l = len(alphabet)
     res = []
-    while True:
-        big_number, rem = divmod(big_number, l)
+    while number > 0:
+        number, rem = divmod(number, l)
         res.append(alphabet[rem])
-        if big_number == 0:
+        if number == 0:
             break
-    return ''.join(res)[::-1]
+    return ''.join(res)[::-1].rjust(padding, '0')
 
 
+def dec(digits, alphabet_dic=alphabet800dic):
+    """Decode digest from base-len(alphabet).
+    
+    See enc() for more info.
+    
+    Parameters
+    ----------
+    digits
+    alphabet_dic
+
+    Returns
+    -------
+
+    """
+    res = 0
+    last = len(digits) - 1
+    base = len(alphabet_dic)
+    for i, d in enumerate(digits):
+        res += alphabet_dic[d] * pow(base, last - i)
+    return res
+
+
+# Useful, but not really used functions. ====================================
 def encrypt(msg_bytes, key_bytes):
     """AES 16 bytes encryption."""
     from Crypto.Cipher import AES
@@ -155,22 +147,6 @@ def decrypt(encrypted_msg, key_bytes):
     return cipher.decrypt(encrypted_msg)
 
 
-def pretty2bytes(digest):
-    """Convert tiny string (20 chars) to bytes."""
-    return dec(digest).to_bytes(16, 'big')
-
-
-def pretty2int(digest, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                                'abcdefghijklmnopqrstuvwxyz'
-                                'ÁÂÄÅÆÉÊËÍÎÏÐÑÓÔÖÚÛÜÝÞßáâäåæçéêëíîïðñóôöøúûüýþ'):
-    """Convert tiny string (20 chars) to bytes."""
-    return dec(digest, alphabet)
-
-
-def prettydigest(bytes_content):
-    return digest2pretty(md5digest(bytes_content))
-
-
 def intlist2bytes(lst):
     """Each int becomes 4 bytes. max=4294967294"""
     return b''.join([n.to_bytes(4, byteorder='big') for n in lst])
@@ -179,10 +155,13 @@ def intlist2bytes(lst):
 def bytes2intlist(bytes_content):
     """Each 4 bytes become an int."""
     n = len(bytes_content)
-    return [bytes2int(bytes_content[i:i + 4]) for i in range(0, n, 4)]
+    return [
+        int.from_bytes(bytes_content[i:i + 4], 'big') for i in range(0, n, 4)
+    ]
 
 
-def pmatrix2pretty(m, alphabet=alphabet1224):
+# Dirty and fast encoders ....
+def pmatrix2pretty(m, alphabet):  # =alphabet1224
     """Convert a permutation matrix to a string using the given alphabet.
 
     The alphabet should have at least |m|² - 1 letters.
@@ -201,24 +180,9 @@ def pmatrix2pretty(m, alphabet=alphabet1224):
     return ''.join(lst)
 
 
-def pretty2pmatrix(text, side=35, alphabet_dict=alphabet1224dic):
+def pretty2pmatrix(text, side, alphabet_dict):  # =alphabet1224dic
     """See pmatrix2pretty."""
     m = [x for d in text[:-1] for x in divmod(alphabet_dict[d], side)]
     if side % 2 == 1:
         m.append(alphabet_dict[text[-1]])
     return m
-
-
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        if obj is not None:
-            from pjdata.aux.uuid import UUID
-            if isinstance(obj, np.ndarray):
-                return str(obj)
-            elif isinstance(obj, UUID):
-                return obj.id
-            elif not isinstance(
-                    obj, (list, set, str, int, float, bytearray, bool)):
-                return obj.jsonable
-
-        return JSONEncoder.default(self, obj)
