f4d726f8531beac03a40347332e8a9e6c69bb656
ealcobaca <e.alcobaca@gmail.com>
2020-06-23 00:11:51 -0300

    Initial implementation to make WithIdentification objects comparable.

--- pjdata/mixin/withidentification.py
+++ pjdata/mixin/withidentification.py
@@ -1,6 +1,6 @@
 """ Identifyable Module. """
 from abc import ABC, abstractmethod
-from functools import cached_property
+from functools import cached_property, lru_cache
 
 from pjdata.aux.uuid import UUID
 
@@ -29,6 +29,9 @@ class WithIdentification(ABC):
         -------
             A unique identifier UUID object.
         """
+        return self._compute_uuid()
+
+    def _compute_uuid(self) -> UUID:
         if self._uuid is None:
             content = self._uuid_impl()
             self._uuid = content if isinstance(content, UUID) else UUID(content.encode())
@@ -58,3 +61,47 @@ class WithIdentification(ABC):
         """Specific internal calculation made by each child class.
 
         Should return a string or a UUID object to be used directly."""
+
+    # Eu tentei essa abordagem, mas infelizmente isso não parece ser interessante
+    # 1) devido ao hash, o python reutiliza as classes e quebra os componentes que usam
+    # 2) o tempo para criar os objetos triplicou
+    #    ex:
+    #     def ger_workflow(arq="iris.arff"):
+    #         np.random.seed(0)
+    #
+    #         workflow = Pipeline(
+    #             File(arq),
+    #             Partition(),
+    #             Map(PCA(), select(SVMC(), DT(criterion="gini")), Metric(enhance=False)),
+    #             Summ(function="mean", enhance=False),
+    #             Reduce(),
+    #             Report("Mean S: $S", enhance=False),
+    #         )
+    #
+    #         return workflow
+
+    #     np.random.seed(0)
+    #     start_time = time.time()
+    #     pipes = rnd(ger_workflow(), n=1000)
+    #     elapsed_time = time.time() - start_time
+    #     print("enlapsed time: ", elapsed_time)
+    #
+    # Portanto, se não conseguirmos solução melhor, acho que o "isequal" será alternativa mais conveniente
+
+    # def __eq__(self, other) -> bool:
+    #     if not hasattr(other, 'uuid'):
+    #         return False
+    #     # TODO: check why compare uuid not work
+    #     # return self.uuid == other.uuid
+    #     return self.uuid.n == other.uuid.n
+    #
+    # def __hash__(self) -> int:
+    #     return self._compute_uuid().n
+
+    def isequal(self, other):
+        if not hasattr(other, 'uuid'):
+            return False
+        # TODO: check why compare uuid not work (I think is something related to self.m)
+        # return self.uuid == other.uuid
+        return self.uuid.n == other.uuid.n
+
